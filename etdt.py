2d4198fe6fd5179591e5cd7027e501067b261e5c
Shuvo <suvo10.info@gmail.com>
2025-09-08 16:47:55 +0600

    ryb yey

---
+++ etdt.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+from tensorflow_graphics.geometry import transformation
+
+print("TensorFlow version:", tf.__version__)
+print("TensorFlow Graphics version:", tf.__version__)
